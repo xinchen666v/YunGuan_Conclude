4eeff1670a46fd6cb07e82e7d2862f42636423b5
xinchen <2019651597@qq.com>
2024-02-27 01:27:07 +0800

    读取MNIST数据集，并使用PyTorch库进行训练和测试

---
+++ Torch.py
@@ -0,0 +1,23 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+import matplotlib.pyplot as plt
+
+
+# 读取MNIST数据集，并使用PyTorch库进行训练和测试。
+class Net(torch.nn.Module):
+    def __init__(self):
+        super().__init__()  # 继承 torch.nn.Module 的构造函数
+
+        # 定义第一层全连接层，输入维度为 28 * 28，输出维度为 64
+        self.fcl = torch.nn.Linear(28 * 28, 64)
+
+        # 定义第二层全连接层，输入维度为 64，输出维度为 64
+        self.fc2 = torch.nn.Linear(64, 64)
+
+        # 定义第三层全连接层，输入维度为 64，输出维度为 64
+        self.fc3 = torch.nn.Linear(64, 64)
+
+        # 定义第四层全连接层，输入维度为 64，输出维度为 10
+        self.fc4 = torch.nn.Linear(64, 10)
