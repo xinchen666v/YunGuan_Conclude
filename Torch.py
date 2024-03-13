da47aa10fa8199cb4d531a23459e0f3ffe81e4a0
xinchen <2019651597@qq.com>
2024-03-13 17:21:23 +0800

    增加了云服务api的python接口的实现方法

--- Torch.py
+++ Torch.py
@@ -33,9 +33,10 @@ class Net(torch.nn.Module):
         x = torch.nn.functional.relu(self.fc3(x))
 
         # 应用 ReLU 激活函数
-        x = torch.nn.functional.log_softmax(self.fc4(x),dim=1)  # 为了提高运算的稳定性，套上了一个对数运算
+        x = torch.nn.functional.log_softmax(self.fc4(x), dim=1)  # 为了提高运算的稳定性，套上了一个对数运算
 
         return x
+
 
 # 定义数据加载器
 def get_data_loader(is_train):
@@ -48,31 +49,31 @@ def evaluate(test_data, net):
     n_correct = 0
     n_total = 0
     with torch.no_grad():
-        for (x, y) in test_data:   # 从测试集中批次取出数据
+        for (x, y) in test_data:  # 从测试集中批次取出数据
             outputs = net.forward(x.view(-1, 28 * 28))  # 计算神经网络的预测值
             for i, output in enumerate(outputs):
                 if torch.argmax(output) == y[i]:  # argmax函数计算一个数列中的最大值的序号
                     n_correct += 1
-                n_total += 1        # 累加正确预测的数量
-    return n_correct / n_total      # 返回正确率
+                n_total += 1  # 累加正确预测的数量
+    return n_correct / n_total  # 返回正确率
 
 
 def main():
     train_data = get_data_loader(is_train=True)
     test_data = get_data_loader(is_train=False)
-    net = Net()   # 初始化神经网络
+    net = Net()  # 初始化神经网络
 
-    print("initial accuracy:", evaluate(test_data, net))    # 打印初始正确率，理论上接近0.1
+    print("initial accuracy:", evaluate(test_data, net))  # 打印初始正确率，理论上接近0.1
 
     # pytorch的固定写法
     optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
-    for epoch in range(2):      # 每个轮次，就是一个epoch，这样可以提高数据集的利用率
+    for epoch in range(2):  # 每个轮次，就是一个epoch，这样可以提高数据集的利用率
         for (x, y) in train_data:
             net.zero_grad()  # 初始化
-            output = net.forward(x.view(-1, 28 * 28))   # 正向传播
-            loss = torch.nn.functional.nll_loss(output, y)      # 计算差值，nll_loss是对数损失函数，是为了匹配上面的log_softmax
-            loss.backward()     # 反向误差传播
-            optimizer.step()        # 优化网络参数
+            output = net.forward(x.view(-1, 28 * 28))  # 正向传播
+            loss = torch.nn.functional.nll_loss(output, y)  # 计算差值，nll_loss是对数损失函数，是为了匹配上面的log_softmax
+            loss.backward()  # 反向误差传播
+            optimizer.step()  # 优化网络参数
         print("epoch", epoch, "accuracy:", evaluate(test_data, net))
 
     # 随机抽取三张图片
